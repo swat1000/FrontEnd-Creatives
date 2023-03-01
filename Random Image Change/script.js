8284dbfe23d60fe466d497f2a7d2530ab9519673
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-03-01 22:36:36 +0530

    Random Image function done

--- Random Image Change/script.js
+++ Random Image Change/script.js
@@ -1,19 +1,15 @@
-function displayRandomImage() {
-    const images = [
-      'Image1.jpg',
-      'Image2.jpg',
-      'Image3.jpg',
-      'Image4.jpg'
-    ];
-  
-    const randomIndex = Math.floor(Math.random() * images.length);
-    const randomImage = images[randomIndex];
-  
-    const imgElement = document.getElementById('random-image');
-    imgElement.src = randomImage;
-  }
-  
-  window.onload = function() {
-    displayRandomImage();
-  };
+const images = document.querySelectorAll("img");
+
+let i = 0;
+
+setInterval(function(){
+   
+ images.forEach( (img, i) => {
+    img.style.display = 'none';
+  })
+
+  images[i].style.display = 'block';
+  i++;
+
+}, 2000);
   
